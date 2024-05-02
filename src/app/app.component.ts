e5d97e19e08e2a0f420d7374cae17e85cbe32171
mathieu <mathieu@MySport.myguest.virtualbox.org>
2024-05-02 17:53:13 +0200

    side bar termine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,61 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { WelcomePanelComponent } from './components/welcome-panel/welcome-panel.component';
 import { ImagePanelComponent } from './components/image-panel/image-panel.component';
 import { HomeComponent } from './components/home/home.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent, 
+    HomeComponent, 
+    MatSidenavModule, 
+    MatIcon, 
+    MatMenuModule, 
+    MatMenuTrigger, 
+    MatMenu, 
+    MatListModule,
+    MatExpansionModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('expandCollapse', [
+      state('open', style({
+        height: '*',
+        opacity: 1
+      })),
+      state('closed', style({
+        height: '0px',
+        opacity: 0
+      })),
+      transition('open <=> closed', [
+        animate('0.6s ease-in-out')
+      ]),
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'jesper';
+
+  isSubmenuOpen = false;
+  isSubmenuGuideOpen = false;
+
+  toggleSubmenu() {
+    this.isSubmenuOpen = !this.isSubmenuOpen;
+  }
+
+  toggleSubmenuGuide() {
+    this.isSubmenuGuideOpen = !this.isSubmenuGuideOpen;
+  }
+  
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { BlueButtonComponent } from '../blue-button/blue-button.component';
 import { WhiteButtonComponent } from '../white-button/white-button.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BlueButtonComponent, WhiteButtonComponent],
+  imports: [BlueButtonComponent, WhiteButtonComponent, MatIconModule, MatButtonModule, MatMenuModule, MatToolbarModule, MatSidenavModule, MatListModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  @Output() openSideNav = new EventEmitter()
 
 }
