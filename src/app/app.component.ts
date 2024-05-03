dcce199dca0595466bd30a5d11b0685e2010f18c
Mathieu jelsch <mathieu.jelsch@gmail.com>
2024-05-03 11:03:20 +0200

    sidebar terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { WelcomePanelComponent } from './components/welcome-panel/welcome-panel.component';
-import { ImagePanelComponent } from './components/image-panel/image-panel.component';
 import { HomeComponent } from './components/home/home.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIcon } from '@angular/material/icon';
-import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
-import { MatListModule } from '@angular/material/list';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @Component({
@@ -20,45 +14,14 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     RouterOutlet,
     NavbarComponent, 
     HomeComponent, 
-    MatSidenavModule, 
-    MatIcon, 
-    MatMenuModule, 
-    MatMenuTrigger, 
-    MatMenu, 
-    MatListModule,
-    MatExpansionModule,
-    CommonModule
+    MatSidenavModule,
+    CommonModule,
+    SidebarComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  animations: [
-    trigger('expandCollapse', [
-      state('open', style({
-        height: '*',
-        opacity: 1
-      })),
-      state('closed', style({
-        height: '0px',
-        opacity: 0
-      })),
-      transition('open <=> closed', [
-        animate('0.6s ease-in-out')
-      ]),
-    ]),
-  ]
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'jesper';
-
-  isSubmenuOpen = false;
-  isSubmenuGuideOpen = false;
-
-  toggleSubmenu() {
-    this.isSubmenuOpen = !this.isSubmenuOpen;
-  }
-
-  toggleSubmenuGuide() {
-    this.isSubmenuGuideOpen = !this.isSubmenuGuideOpen;
-  }
   
 }
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,51 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { AfterViewChecked, Component, ElementRef, ViewChild } from '@angular/core';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatIconModule,
+    MatIcon,
+    MatMenuTrigger,
+    MatMenuModule,
+    CommonModule,
+    MatListModule
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss',
+  animations: [
+    trigger('expandCollapse', [
+      state('open', style({
+        height: '*' ,
+        opacity: 1
+      })),
+      state('closed', style({
+        height: '0px',
+        opacity: 0
+      })),
+      transition('open <=> closed', [
+        animate('0.6s ease-in-out')
+      ]),
+    ]),
+  ]
+})
+export class SidebarComponent {
+
+  isSubmenuOpen = false;
+  isSubmenuGuideOpen = false;
+
+  toggleSubmenu() {
+    this.isSubmenuOpen = !this.isSubmenuOpen;
+  }
+
+  toggleSubmenuGuide() {
+    this.isSubmenuGuideOpen = !this.isSubmenuGuideOpen;
+  }
+}
